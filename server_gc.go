490a9ce3d78309115decc30c14ddb620fe5f2f59
bcosso <bcosso@gmail.com>
2022-10-17 15:59:01 +0200

    Add files via upload

---
+++ server_gc.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"io/ioutil"
+	"encoding/json"
+)
+
+
+
+const data_interval = 10000
+const wal_interval = 1000
+
+
+func dump_data(s string) {
+	for (true){
+		time.Sleep(data_interval * time.Millisecond)
+		dump_it(s)
+		dump_mt(s)
+	}
+}
+
+func dump_mt(s string) {
+	fmt.Println(s)
+	fmt.Println(mt)
+}
+
+func dump_wal(s string) {
+	for (true){
+		time.Sleep(wal_interval * time.Millisecond)
+		file, _ := json.MarshalIndent(wal, "", " ")
+ 		_ = ioutil.WriteFile("wal_file.json", file, 0644)
+		fmt.Println(s)
+		fmt.Println(mt)
+	}
+}
+
+func dump_it(s string) {
+	fmt.Println(s)
+	fmt.Println(mt)
+}
