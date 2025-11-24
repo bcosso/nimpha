b39acbac7500287b984f29813004133b8663333a
bcosso <bcosso@gmail.com>
2025-11-24 18:11:58 +0000

    First changes to add schema system

--- src/schema_manager.go
+++ src/schema_manager.go
@@ -9,11 +9,41 @@ func (sing *SingletonTable) InitializeSchema() {
 	//Check if the table exists. If it does not, create it.
 
 	//Add Hash Index to __sys_table on the column table_name
+	var index TableIndex
+	index.ColumnName = _columnTable
+	index.TableName = _schemaTable
+	index.IndexType = "HASH"
+
+	singletonIndex.AttachNewHashIndex(index)
 
 }
 
-func (sing *SingletonTable) CreateSchema() {
+type Schema struct {
+	TableName      string
+	Column         map[string]interface{}
+	Identity       bool
+	ColumnIdentity string
+}
+
+func (sing *SingletonTable) CreateSchema(schema Schema) {
 	//creates a new entry in the table __sys_table if it doesn't exist (search index)
+	var rowSchema mem_row
+	if _, hasIndex := singletonIndex.hashIndex[_schemaTable]; hasIndex {
+		if _, hasIndex = singletonIndex.hashIndex[_schemaTable][_columnTable][schema.TableName]; hasIndex {
+			rowSchema = *(singletonIndex.hashIndex[_schemaTable][_columnTable][schema.TableName])
+		}
+	} else {
+		rowSchema.Table_name = _schemaTable
+		if schema.ColumnIdentity != "" {
+			rowSchema.Parsed_Document["_identity_value"] = 0
+			rowSchema.Parsed_Document["_identity_column"] = schema.ColumnIdentity
+		}
+		//rowSchema.Parsed_Document = schema
+		sing.mt[_schemaTable] = append(sing.mt[_schemaTable], &rowSchema)
+		singletonIndex.AttachNewHashIndexUnity(_schemaTable, _columnTable, schema.ColumnIdentity, &rowSchema)
+
+	}
+
 	//columns : table_name (string), _identity_value(int), _identity_column (string),
 	//	    table_columns map[string]interface
 
